bc3953424d9df63b1934677c1c2d21de2706b679
andreasmilants <andreas.milants@gmail.com>
2020-10-24 23:03:38 +0200

    plate animation

--- lib/appetite/select_people.dart
+++ lib/appetite/select_people.dart
@@ -4,6 +4,7 @@ import 'package:abc_cooking/services/service.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'dart:math';
 
 class SelectPeopleWidget extends StatefulWidget {
   final Recipe recipe;
@@ -18,11 +19,15 @@ class SelectPeopleWidget extends StatefulWidget {
 
 class _SelectPeopleState extends State<SelectPeopleWidget> {
   int people;
+  PlatesWidget platesWidget;
 
   @override
   void initState() {
     super.initState();
     people = AppetiteService().getPeople();
+    platesWidget = PlatesWidget(
+      startPlates: people,
+    );
   }
 
   @override
@@ -34,11 +39,13 @@ class _SelectPeopleState extends State<SelectPeopleWidget> {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Text(
-            'Persons',
-            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 31),
+          platesWidget,
+          SizedBox(
+            height: 20,
           ),
-          SizedBox(height: 30,),
+          SizedBox(
+            height: 30,
+          ),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
@@ -49,6 +56,7 @@ class _SelectPeopleState extends State<SelectPeopleWidget> {
                     ? () {
                         setState(() {
                           people -= 1;
+                          platesWidget.removePlate();
                         });
                       }
                     : null,
@@ -83,6 +91,7 @@ class _SelectPeopleState extends State<SelectPeopleWidget> {
                     ? () {
                         setState(() {
                           people += 1;
+                          platesWidget.addPlate();
                         });
                       }
                     : null,
@@ -123,4 +132,235 @@ class _SelectPeopleState extends State<SelectPeopleWidget> {
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
+}
+
+class PlatesWidget extends StatefulWidget {
+  final int startPlates;
+  _PlatesState state;
+
+  PlatesWidget({this.startPlates: 1});
+
+  @override
+  State<StatefulWidget> createState() {
+    state = _PlatesState(startPlates);
+    return state;
+  }
+
+  void addPlate() {
+    state.addPlate();
+  }
+
+  void removePlate() {
+    state.removePlate();
+  }
+}
+
+class _PlatesState extends State<PlatesWidget> with TickerProviderStateMixin {
+  List<Plate> plates = [];
+  AnimationController controller;
+  Animation<double> _radiusPlate;
+  Animation<double> _radiusTable;
+
+  int _amountPlates;
+
+  _PlatesState(this._amountPlates);
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 400),
+    );
+
+    var arcSize = 2 * pi / _amountPlates;
+    var startTheta = 0.0;
+    for (var i = 0; i < _amountPlates; i++) {
+      Tween<double> tweenTheta = Tween(begin: startTheta, end: startTheta);
+      var animationTheta = tweenTheta.animate(controller)
+        ..addListener(() {
+          setState(() {});
+        });
+
+      plates.add(Plate(animationTheta));
+      startTheta += arcSize;
+    }
+    var r = .7 + .006 * _amountPlates;
+    Tween<double> tweenTable = Tween(begin: r, end: r);
+    _radiusTable = tweenTable.animate(controller)
+      ..addListener(() {
+        setState(() {});
+      });
+    var radius = min(r * pi / _amountPlates * .75, .2);
+    Tween<double> tweenPlate = Tween(begin: radius, end: radius);
+    _radiusPlate = tweenPlate.animate(controller)
+      ..addListener(() {
+        setState(() {});
+      });
+
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Transform(
+          alignment: Alignment.center,
+          transform: Matrix4.skew(0, 0)..rotateX(pi/2.75),
+          child: CustomPaint(
+            foregroundPainter:
+                PlatePainter(_radiusTable, _radiusPlate, plates: plates),
+            child: Container(
+              decoration:
+                  BoxDecoration(shape: BoxShape.circle, color: Colors.black54),
+              width: 300,
+              height: 300,
+            ),
+          ),
+        )
+      ],
+    );
+  }
+
+  void setRadius(AnimationController controller) {
+    var r = .7 + .006 * _amountPlates;
+    Tween<double> tweenTable = Tween(begin: _radiusTable.value, end: r);
+    _radiusTable = tweenTable.animate(controller)
+      ..addListener(() {
+        setState(() {});
+      });
+    var radius = min(r * pi / _amountPlates * .75, .2);
+    Tween<double> tweenPlate = Tween(begin: _radiusPlate.value, end: radius);
+    _radiusPlate = tweenPlate.animate(controller)
+      ..addListener(() {
+        setState(() {});
+      });
+    setState(() {});
+    print(_radiusTable);
+  }
+
+  void addPlate() {
+    controller.dispose();
+    controller = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 400),
+    );
+    _amountPlates += 1;
+    var arcSize = 2 * pi / _amountPlates;
+    var startTheta = arcSize;
+    for (var plate in plates) {
+      Tween<double> tweenTheta =
+          Tween(begin: plate.animationTheta.value, end: startTheta);
+      var animationTheta = tweenTheta.animate(controller)
+        ..addListener(() {
+          setState(() {});
+        });
+      plate.animationTheta = animationTheta;
+      startTheta += arcSize;
+    }
+
+    Tween<double> tweenTheta = Tween(begin: 2 * pi, end: 2 * pi);
+    var animationTheta = tweenTheta.animate(controller)
+      ..addListener(() {
+        setState(() {});
+      });
+    plates.add(Plate(animationTheta));
+    setRadius(controller);
+    setState(() {});
+    controller.forward();
+  }
+
+  void removePlate() {
+    controller.dispose();
+    controller = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 400),
+    );
+    _amountPlates -= 1;
+    var arcSize = 2 * pi / _amountPlates;
+    var startTheta = 0.0;
+    for (var plate in plates) {
+      Tween<double> tweenTheta =
+          Tween(begin: plate.animationTheta.value, end: startTheta);
+      var animationTheta = tweenTheta.animate(controller)
+        ..addListener(() {
+          setState(() {});
+        });
+      plate.animationTheta = animationTheta;
+      startTheta += arcSize;
+    }
+    plates.removeAt(plates.length - 1);
+    setRadius(controller);
+    setState(() {});
+    controller.forward();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+}
+
+class PlatePainter extends CustomPainter {
+  final List<Plate> plates;
+  final Animation<double> radiusPlate;
+  final Animation<double> radiusTable;
+
+  PlatePainter(
+    this.radiusTable,
+    this.radiusPlate, {
+    this.plates: const [],
+  });
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    var distanceFromCenter = radiusTable.value;
+    var radius = radiusPlate.value * size.width / 2;
+
+    var paintCenter = Paint()
+      ..color = Colors.grey[300]
+      ..strokeWidth = 5
+      ..style = PaintingStyle.fill
+      ..strokeCap = StrokeCap.round;
+    var paintRim = Paint()
+      ..color = Colors.white
+      ..strokeWidth = 5
+      ..style = PaintingStyle.fill
+      ..strokeCap = StrokeCap.round;
+
+    for (var plate in plates) {
+      Offset center = Offset(
+          size.width / 2 +
+              size.width *
+                  distanceFromCenter /
+                  2 *
+                  cos(plate.animationTheta.value - pi / 2),
+          size.height / 2 +
+              size.height *
+                  distanceFromCenter /
+                  2 *
+                  sin(plate.animationTheta.value - pi / 2));
+      canvas.drawCircle(center, radius, paintRim);
+      canvas.drawCircle(center, radius * .75, paintCenter);
+    }
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    for (var plate in plates) {
+      if (!plate.animationTheta.isCompleted) {
+        return true;
+      }
+    }
+    return false;
+  }
+}
+
+class Plate {
+  Animation<double> animationTheta;
+
+  Plate(this.animationTheta);
 }
